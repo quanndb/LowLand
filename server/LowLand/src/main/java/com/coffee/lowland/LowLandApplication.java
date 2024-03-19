cac6cf056e21421db5a8a49d9a4aa537ea1389fb
quanndb <quan2082003@gmail.com>
2024-03-19 23:21:19 +0700

    Base source

---
+++ server/LowLand/src/main/java/com/coffee/lowland/LowLandApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.lowland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLandApplication.class, args);
+	}
+
+}
---
+++ server/LowLand/src/test/java/com/coffee/lowland/LowLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.lowland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
